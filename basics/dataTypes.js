abdd189c57860595b5eeaf09a084cac0f9f5ffea
anishshetty6 <71397185+anishshetty6@users.noreply.github.com>
2024-01-02 12:38:42 +0000

    dataTypes learnt

---
+++ basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict " //treat all js as that of newer version
+
+let score1= "33abc"
+let score2=33
+console.log(typeof(score2))
+console.log(typeof(score1))
+let valInNum=Number(score1)
+console.log(typeof(valInNum))
+console.log(valInNum)
+
+//"33"=>33
+//"33abc"=> NaN
+//true=1;false=0
+
+let isLoggedIn="anish"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1=>true;0=>false
+//""=>false
+//"anish"=>true
+
+let someNum=11
+let stringNum=String(someNum)
+console.log(stringNum)
+console.log(typeof stringNum)
