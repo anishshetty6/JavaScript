9b1101cca52b82a858d6e4f28abf079cb2034ae8
anishshetty6 <71397185+anishshetty6@users.noreply.github.com>
2024-01-23 10:33:01 +0000

    objects part2

---
+++ basics/objects2.js
@@ -0,0 +1,18 @@
+//const tinderUser=new Object() singleton object
+const tinderUser = {}
+
+tinderUser = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLog = false
+//console.log(tinderUser);
+const regularUser = {
+    email: "alt@gmail.com",
+    fullname :  {
+        userfullname: {
+            firstname: "nishant",
+            lastname: "panchal"
+        }
+    }
+}
+
+
