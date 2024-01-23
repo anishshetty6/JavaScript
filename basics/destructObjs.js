856ddc79d5be3a97460db9cb0c321bf65bed4e86
anishshetty6 <71397185+anishshetty6@users.noreply.github.com>
2024-01-23 10:39:06 +0000

    learnt about destructuring objects

---
+++ basics/destructObjs.js
@@ -0,0 +1,11 @@
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseInst:"ARJ"
+
+}
+
+const {courseInst: inst}=course         //destructuring an object
+
+console.log(inst)
+
